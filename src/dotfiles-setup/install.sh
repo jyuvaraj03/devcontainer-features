7bd9c7a8eb50c26a166dd6a9c5dbf53ccb9d8223
Yuvaraj J <jyuvaraj03@gmail.com>
2024-09-28 04:37:08 +0530

    Add release dotfiles link

--- src/dotfiles-setup/install.sh
+++ src/dotfiles-setup/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Download dotfiles from https://github.com/jyuvaraj03/dotfiles/archive/refs/heads/main.zip
-wget https://github.com/jyuvaraj03/dotfiles/archive/refs/heads/main.zip
+wget https://github.com/jyuvaraj03/dotfiles/archive/refs/tags/v0.0.1.zip
 # unzip as dotfiles
 unzip main.zip -d dotfiles
 cd dotfiles
